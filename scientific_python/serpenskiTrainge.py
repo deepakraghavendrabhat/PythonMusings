a55b5f81df6927c704b31a488273defc678b1af6
deepakraghavendrabhat <deepakbhat.r@gmail.com>
2022-10-12 22:47:14 +0530

    Add

---
+++ scientific_python/serpenskiTrainge.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 100000
+
+#initialize x and y vectors
+sx = np.zeros(N)
+sy = np.zeros(N)
+
+for i in range(1,N):
+
+    # generate a random number {1,2,3}
+    k = np.random.randint(1,4)
+    
+    # update x and y plots
+    sx[i] = sx[i-1]/2 + k - 1
+    sy[i] = sy[i-1]/2
+    if k ==2:
+        sy[i] += 2
+        
+#plotting the points
+
+plt.plot( sx , sy, 'k.', markersize = 1)
+plt.axis('off')
+plt.show()
+
